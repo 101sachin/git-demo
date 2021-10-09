1b445a731e641a22f528aa3773e9c9e678ced1f9
sachintyagi-azusol <sachin.tyagi@flashonmind.com>
2021-10-09 23:56:08 +0530

    index.php changed to index layout page

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
-<?php 
-
-[A	[D[D[D[Decho ""[Dindex file of root directory";
+<?php
+echo "index file of git-demo project";
 ?>
-[A[C[C
-new line added in to the index file.
-but its produced the error code[A[B.
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>
+        new line added in to the index file.
+        but its produced the error code.
+    </p>
+</body>
+
+</html>
